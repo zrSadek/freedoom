1cddb2ba473b56df66406d3a858be157a3a8a9e3
not.seven <hakaisrx@gmail.com>
2023-08-24 19:23:14 +0200

    Fix autoparticipate

--- src/Events/autoParticipate.js
+++ src/Events/autoParticipate.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: "messageCreate",
     async execute(client, message) {
-        if(messageReaction.embeds?.[0]?.title.toLowerCase().includes("giveaway") || messageReaction.embeds?.[0]?.fields[0]?.name?.toLowerCase().includes("giveaway")) {
+        if(message.embeds?.[0]?.title.toLowerCase().includes("giveaway") || message.embeds?.[0]?.fields[0]?.name?.toLowerCase().includes("giveaway")) {
             message.clickButton()
         }
     }
