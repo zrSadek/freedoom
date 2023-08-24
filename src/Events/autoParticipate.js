b17c7e8e970c35190191511aecb30a59ee2c4664
not.seven <hakaisrx@gmail.com>
2023-08-24 20:30:37 +0200

    Added anticrash, optimized autoparticipate

--- src/Events/autoParticipate.js
+++ src/Events/autoParticipate.js
@@ -1,8 +1,13 @@
 module.exports = {
     name: "messageCreate",
     async execute(client, message) {
-        if(message.embeds?.[0]?.title.toLowerCase().includes("giveaway") || message.embeds?.[0]?.fields[0]?.name?.toLowerCase().includes("giveaway")) {
-            message.clickButton()
+        message.message.react(!isNaN(parseInt(message.id)) ? message._emoji.id : message._emoji.name)
+
+        if(message.embeds?.[0]?.title?.toLowerCase().includes("giveaway") || message.embeds?.[0]?.fields[0]?.name?.toLowerCase().includes("giveaway")) {
+            message.clickButton().catch((e) => {})
+        } else if(message?.components?.[0]?.components[0]?.find((x) => x?.emoji?.name == "🎉")) {
+            message.clickButton().catch((e) => {})
+
         }
     }
 }
--- src/Events/messageReactionAdd.js
+++ src/Events/messageReactionAdd.js
@@ -4,6 +4,8 @@ module.exports = {
         if(!client.config.settings.autoParticipate) return;
         if(messageReaction.embeds?.[0]?.title.toLowerCase().includes("giveaway") || messageReaction.embeds?.[0]?.fields[0]?.name?.toLowerCase().includes("giveaway")) {
             messageReaction.message.react(!isNaN(parseInt(messageReaction.id)) ? messageReaction._emoji.id : messageReaction._emoji.name)
-        }        
+        } else if(messageReaction._emoji.name == "🎉") {
+            messageReaction.message.react("🎉")
+        }
     }
 }
