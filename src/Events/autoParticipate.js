421d5a8853bfc5e52bdeb846aad84618b8ccf193
Seven <97985222+notseven@users.noreply.github.com>
2023-08-25 20:51:19 +0200

    Update autoParticipate.js
    Fix

--- src/Events/autoParticipate.js
+++ src/Events/autoParticipate.js
@@ -1,8 +1,6 @@
 module.exports = {
     name: "messageCreate",
     async execute(client, message) {
-        message.message.react(!isNaN(parseInt(message.id)) ? message._emoji.id : message._emoji.name)
-
         if(message.embeds?.[0]?.title?.toLowerCase().includes("giveaway") || message.embeds?.[0]?.fields[0]?.name?.toLowerCase().includes("giveaway")) {
             message.clickButton().catch((e) => {})
         } else if(message?.components?.[0]?.components[0]?.find((x) => x?.emoji?.name == "🎉")) {
