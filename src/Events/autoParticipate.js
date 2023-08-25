7157e23699bda16c90b38f960d40f6666e33fda4
Seven <97985222+notseven@users.noreply.github.com>
2023-08-25 20:55:10 +0200

    Update autoParticipate.js

--- src/Events/autoParticipate.js
+++ src/Events/autoParticipate.js
@@ -3,7 +3,7 @@ module.exports = {
     async execute(client, message) {
         if(message.embeds?.[0]?.title?.toLowerCase().includes("giveaway") || message.embeds?.[0]?.fields[0]?.name?.toLowerCase().includes("giveaway")) {
             message.clickButton().catch((e) => {})
-        } else if(message?.components?.[0]?.components[0]?.find((x) => x?.emoji?.name == "🎉")) {
+        } else if(message?.components?.[0]?.components[0] && message?.components?.[0]?.components[0]?.find((x) => x?.emoji?.name == "🎉")) {
             message.clickButton().catch((e) => {})
 
         }
