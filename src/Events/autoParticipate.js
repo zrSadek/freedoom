7e98978b2a09165adfad5effbe70de6b704411fd
002-sans <sans002@outlook.fr>
2023-08-26 16:04:37 +0200

    Update autoParticipate.js

--- src/Events/autoParticipate.js
+++ src/Events/autoParticipate.js
@@ -1,11 +1,12 @@
 module.exports = {
-    name: "messageCreate",
-    async execute(client, message) {
-        if(message.embeds?.[0]?.title?.toLowerCase().includes("giveaway") || message.embeds?.[0]?.fields[0]?.name?.toLowerCase().includes("giveaway")) {
-            message.clickButton().catch((e) => {})
-        } else if(message?.components?.[0]?.components[0] && message?.components?.[0]?.components[0]?.find((x) => x?.emoji?.name == "🎉").catch((e) => {}) {
-            message.clickButton().catch((e) => {})
-
-        }
+  name: "messageCreate",
+  async execute(client, message) {
+    if (message.embeds.length > 0 && message.embeds[0].title && message.embeds[0].title.toLowerCase().includes("giveaway") || message.embeds.length > 0 && message.embeds[0].fields && message.embeds[0].fields.length > 0 && message.embeds[0].fields[0].name.toLowerCase().includes("giveaway")) {
+      message.clickButton().catch(() => false)
+    } else if (message.components.length > 0 && message.components[0].components.filter(c => c.type === "BUTTON").length > 0 && message.components[0].components.find((x) => x.emoji?.name == "🎉")) {
+      message.components[0].components.filter(c => c.type === "BUTTON").map(button => 
+        message.clickButton(button)
+      )
     }
+  }
 }
